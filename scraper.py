15752f31955cc6fd0ad057f11b88850af9e673b4
cjredmond <connor.redmond@gmail.com>
2016-11-16 14:44:32 -0500

    started using espn bottom line to scrape

--- scraper.py
+++ scraper.py
@@ -2,4 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 base_url_nfl = 'http://www.espn.com/nfl/bottomline/scores'
 data = requests.get(base_url_nfl)
-souper = BeautifulSoup(data.text, 'html.parser')
+souper_nfl = BeautifulSoup(data.text, 'html.parser')
+string_soup_nfl = str(souper_nfl)
+split_soup_nfl = string_soup_nfl.split('http://sports.espn.go.com/nfl/preview?gameId=')
+for x in split_soup_nfl:
+    # print(x)
+    # print('\n')
+    pass
+
+base_url_nba = 'http://www.espn.com/nba/bottomline/scores'
+data = requests.get(base_url_nba)
+souper_nba = BeautifulSoup(data.text, 'html.parser')
+string_soup_nba = str(souper_nba)
+
+split_soup_nba = string_soup_nba.split('gameId')
+for x in split_soup_nba:
+    print(x)
+    print('\n')
