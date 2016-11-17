be95e322407184cbc9fbc44f875a15c77ef9807d
cjredmond <connor.redmond@gmail.com>
2016-11-16 22:26:25 -0500

    nba scraper works

--- scraper.py
+++ scraper.py
@@ -1,22 +1,23 @@
 import requests
 import datetime
+import urllib
 from bs4 import BeautifulSoup
-base_url_nfl = 'http://www.espn.com/nfl/bottomline/scores'
-data = requests.get(base_url_nfl)
-souper_nfl = BeautifulSoup(data.text, 'html.parser')
-string_soup_nfl = str(souper_nfl)
-split_soup_nfl = string_soup_nfl.split('http://sports.espn.go.com/nfl/preview?gameId=')
-for x in split_soup_nfl:
-    # print(x)
-    # print('\n')
-    pass
+from urllib.parse import parse_qs
 
-base_url_nba = 'http://www.espn.com/nba/bottomline/scores'
-data = requests.get(base_url_nba)
-souper_nba = BeautifulSoup(data.text, 'html.parser')
-string_soup_nba = str(souper_nba)
 
-split_soup_nba = string_soup_nba.split('gameId')
-for x in split_soup_nba:
-    print(x)
-    print('\n')
+def nba_scores():
+    base_url_nba = 'http://www.espn.com/nba/bottomline/scores'
+    data = requests.get(base_url_nba)
+    x = urllib.parse.unquote(data.text)
+    d = "nba_s_left"
+    game = [d+e for e in data.text.split(d)]
+    game_scores = []
+    for g in game[1:]:
+        current_score = {}
+        for k,v in parse_qs (g).items():
+            if k.startswith('nba_s_left'):
+                current_score['scores'] = v
+            if k.startswith('nba_s_url'):
+                current_score['id'] = v
+        game_scores.append(current_score)
+    return game_scores
